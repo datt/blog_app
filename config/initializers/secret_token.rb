a2316425d5a4fd6446e1e3342903de9a504b6254
Sharang Dashputre <sharang.dashputre@weboniselab.com>
2014-01-02 23:21:51 +0530

    MVC Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogApp::Application.config.secret_key_base = 'bca5dac2b49059c296f81058ad281c94b6862dd70ffd1a346c4db9bc92f2977c9a0beac4fcb78089f0927f399c2e620b1550a743b1e471014bc9865b4cf62a50'
